80205b806a20094f8126398779bd69e843f4600b
moreprajakta0609 <46739422+moreprajakta0609@users.noreply.github.com>
2019-01-30 20:09:46 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,80 @@
+<div class="split right">
+      <form name ="form1" method="post"   onsubmit="return fncSubmit();">
+  <h1>Registration</h1>
+  <table border="0">
+    <tr>
+      <td>
+        First Name*   </td>
+      <td><input type="text" name="firstName" size="35" required></td><br>
+    </tr>
+    <tr>
+      <td>Last Name*</td>
+      <td>
+        <input type="text" name="lastname" size="35" required></td>
+    </tr>
+    <tr>
+      <td>Password*</td>
+      <td> <input type="password" name="password" size="35" required></td>
+    </tr>
+    <tr>
+      <td>Confirm Password*</td>
+      <td><input type="password" name="cpassword" size="35" required></td>
+    </tr>
+    <tr>
+      <td>Date of Birth*</td>
+      <td><input type="number" name="date" style="width:38px">
+          <input type="number" name="month" style="width:38px">
+          <input type="number" name="year" style="width:53px">
+        &nbsp;&nbsp;Gender <select>
+          <option value="male" style="font-weight:bold">Male</option>
+          <option value="female" style="font-weight:bold">Female</option>
+        </select></td>
+    </tr>
+    <tr>
+      <td>Mobile/Cell No.*</td>
+      <td>
+        <input type="int" name="mob" size="35" required></td>
+    </tr>
+    <tr>
+      <td>Email*</td>
+      <td><input type="email" name="mail" size="35" required></td>
+    </tr>
+    <tr>
+      <td>Confirm E-mail*</td>
+      <td><input type="email" name="cmail" size="35" required></td>
+    </tr>
+    <tr>
+      <td>Address*</td>
+      <td><input type="text"  style="height:50px" size="35" required ></td>
+    </tr>
+    <tr>
+      <td></td>
+      <td>City*&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;State*</td>
+    </tr>
+    <tr>
+      <td></td>
+      <td> <input type="text" name="cityname" size="11">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="text" name="sname"
+          size="11"></td>
+    </tr>
+    <tr>
+      <td></td>
+      <td>Zip/Postal Code*<br><input type="text" name="zip" size="35" required></td>
+    </tr>
+    <tr>
+      <td></td>
+      <td>Country*<br>
+        <input type="text" name="Country" size="35" required></td>
+    </tr>
+    <tr>
+      <td></td>
+      <td><input type="checkbox" name="agree" id="Agreed">Agreed to Terms and Conditions</td>
+    </tr>
+    <tr>
+      <td></td>
+    <td><input type="submit" name="submit" value="Sign Up" class=button1></td>
+    </tr>
+  
+</table>
+
+</div>
+
